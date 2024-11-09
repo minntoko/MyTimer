888c4aa3f8daaad8a47f6c1dec5029102a185253
minntoko <minntoko0822@gmail.com>
2024-11-09 23:21:18 +0900

    add: タイマーの初期画面を追加

--- MyTimer/ContentView.swift
+++ MyTimer/ContentView.swift
@@ -9,13 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack{
+                Image("backgroundTimer")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                VStack(spacing: 30.0) {
+                    Text("残り10秒")
+                        .font(.largeTitle)
+                    HStack {
+                        Button {
+                            // ボタンをタップした時のアクション
+                        } label: {
+                            Text("スタート")
+                                .font(.title)
+                                .foregroundColor(.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color("startColor"))
+                                .clipShape(Circle())
+                        }
+                        Button {
+                            // ボタンをタップした時のアクション
+                        } label: {
+                            Text("ストップ")
+                                .font(.title)
+                                .foregroundColor(.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color("stopColor"))
+                                .clipShape(Circle())
+                        }
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink{
+                        SettingView()
+                    } label: {
+                        Text("秒数設定")
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
---
+++ MyTimer/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  MyTimer
+//
+//  Created by 松田知也 on 2024/11/09.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text("設定画面です")
+    }
+}
+
+#Preview {
+    SettingView()
+}
