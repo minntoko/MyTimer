a3a3b6dca1fc2c94418e79de1993d9588febf9a7
minntoko <minntoko0822@gmail.com>
2024-11-09 18:53:16 +0900

    Initial Commit

---
+++ MyTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyTimer
+//
+//  Created by 松田知也 on 2024/11/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by 松田知也 on 2024/11/09.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
